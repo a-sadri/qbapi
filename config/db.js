f0976e7190d6361f6e3b818d7444b220b75962ee
Ali <asadri721@gmail.com>
2022-09-08 19:28:29 +0430

    Setup database and express router

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import chalk from 'chalk';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(chalk.cyan(`MongoDB Connected: ${conn.connection.host}`));
+  } catch (error) {
+    console.log(chalk.red.underline(`Error: ${error.message}`));
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import chalk from 'chalk';
+
+import connectDB from './config/db.js';
+import categoryRoutes from './routes/categoryRoutes.js';
 
 dotenv.config();
+
+// Connect to database
+connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// API Routes
+app.use('/api/v1/categories', categoryRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+  console.log(
+    chalk.cyan(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  );
 });
