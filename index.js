49b35884094d1f15cb17b8ef1629361a13e94c0b
Ali <asadri721@gmail.com>
2022-09-08 21:58:28 +0430

    Implement swagger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
 import connectDB from './config/db.js';
 import { errorhandler } from './middlewares/errorMiddleware.js';
@@ -15,9 +17,30 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+// Swagger config
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'QB API',
+      version: '1.0.0',
+      description: 'An API for Quizzes, Questions and Answers, ... ',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsDoc(options);
+
 const PORT = process.env.PORT || 5000;
 
 // API Routes
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 app.use('/api/v1/categories', categoryRoutes);
 
 app.use(errorhandler);
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -7,7 +7,71 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Category:
+ *      type: object
+ *      required:
+ *        - name
+ *      properties:
+ *        name:
+ *          type: string
+ *          description: The category name
+ *      example:
+ *        name: javascript
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Categories
+ *  description: The Categories managing API
+ */
+
+/**
+ * @swagger
+ * /api/v1/categories:
+ *  get:
+ *    summary: Return the list of all the categories
+ *    tags: [Categories]
+ *    responses:
+ *      200:
+ *        description: The list of the categories
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Category'
+ */
+
 router.get('/', getAllCategory);
+
+/**
+ * @swagger
+ * /api/v1/categories:
+ *  post:
+ *    summary: Create a new category
+ *    tags: [Categories]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Category'
+ *    responses:
+ *     201:
+ *       description: The category was successfully created
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Category'
+ *     500:
+ *      description: Some server error
+ *
+ */
 
 router.post('/', newCategory);
 
