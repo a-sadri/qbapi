0c96669835c74025b99a9f9be1aeccab0b4af6dc
a-sadri <asadri721@gmail.com>
2022-11-30 10:39:40 +0330

    Implement jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/User.js';
+import generateToken from '../utils/generateToken.js';
 
 const register = asyncHandler(async (req, res) => {
   const { name, email, password, role } = req.body;
@@ -13,7 +14,12 @@ const register = asyncHandler(async (req, res) => {
     role,
   });
 
-  res.status(201).json({ success: true });
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    token: generateToken(user._id),
+  });
 });
 
 export { register };
